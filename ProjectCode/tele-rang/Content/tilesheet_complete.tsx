c7ac11fbb0624071718bc2e1977b0c9d25bf9a78
McKane Searle <50674396+Macblast@users.noreply.github.com>
2021-08-27 16:12:11 -0600

    Level

---
+++ ProjectCode/tele-rang/Content/tilesheet_complete.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tilesheet_complete" tilewidth="64" tileheight="64" tilecount="540" columns="27">
+ <image source="tilesheet_complete.png" trans="ffffff" width="1728" height="1280"/>
+</tileset>
