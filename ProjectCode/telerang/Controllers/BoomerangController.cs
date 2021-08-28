a86bc07f699d0893553965d370b6d0c9c549c05d
Shankar Siddharth <7028991+shankarsiddharth@users.noreply.github.com>
2021-08-27 18:05:04 -0600

    Test Camera

---
+++ ProjectCode/telerang/Controllers/BoomerangController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MonoGame.Extended;
+using MonoGame.Extended.Input.InputListeners;
+
+namespace telerang
+{
+    class BoomerangController
+    {
+        private readonly TouchListener _touchListener = new TouchListener();
+        private readonly GamePadListener _gamePadListener = new GamePadListener();
+        private readonly KeyboardListener _keyboardListener = new KeyboardListener();
+        private readonly MouseListener _mouseListener = new MouseListener();
+
+        public void Initialize()
+        {
+            //Components.Add(new InputListenerComponent(this, _keyboardListener, _gamePadListener, _mouseListener, _touchListener));
+        }
+    }
+}
